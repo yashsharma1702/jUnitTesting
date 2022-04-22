e778d715def0d6244aed2b6471a0c6bf7239ea1c
yashsharma1702 <83951470+yashsharma1702@users.noreply.github.com>
2022-04-22 16:27:18 +0530

    jUnit

---
+++ src/jUnitTestPackage/jUnitFunctions.java
@@ -0,0 +1,12 @@
+package jUnitTestPackage;
+
+public class jUnitFunctions {
+	public int addNumbers(int n1, int n2)
+	{
+		return n1+n2;
+	}
+	public String addString(String s1, String s2)
+	{
+		return s1+s2;
+	}
+}
